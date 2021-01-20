b55a32e591555a0b7e555389bf5c6940f1c6137a
EgaHa97 <71167224+EgaHa97@users.noreply.github.com>
2021-01-20 15:45:20 +0200

    Create Reverted Caesar-cipher.py
    in case you need to decrypt the cipher x-)
    
    it's the same code just in line 11 you need to change + on -

---
+++ Reverted Caesar-cipher.py
@@ -0,0 +1,14 @@
+def encrypt(string):
+   alf = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+   new = []
+   for i in string:
+       if i not in alf:
+           new.append(i)
+       else:
+           if  i == "z":
+               new.append("a")
+           else:
+               new.append(alf[alf.index(i)-3])
+   return "".join(new)
+word = input('Enter a word for encryption')
+print(encrypt(word))
